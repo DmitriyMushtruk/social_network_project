09bb99327a7de7b8421029143059cca137018fbd
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-07-25 21:50:00 +0200

    Initial commit

---
+++ social_backend/account/models.py
@@ -0,0 +1,6 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.db import models
+from django.utils import timezone
+
---
+++ social_frontend/src/views/FeedView.vue
@@ -0,0 +1,139 @@
+<template>
+    <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+        <div class="main-left col-span-1">
+            <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
+                <img src="https://i.pravatar.cc/300?img=26" class="mb-6 rounded-full">
+
+                <p><strong>Maria Stein</strong></p>
+
+                <div class="mt-6 flex space-x-8 justify-around">
+                    <p class="text-xs text-gray-500">182 friends</p>
+                    <p class="text-xs text-gray-500">120 posts</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="main-center col-span-2 space-y-4">
+            <div class="bg-white border border-gray-200 text-center rounded-lg">
+                <div class="p-4">
+                    <textarea class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you thinking about?"></textarea>
+                </div>
+
+                <div class="p-4 border-t border-gray-100 flex justify-between">
+                    <a href="#" class="inline-block py-4 px-6 bg-gray-600 text-white rounded-lg">Attach image</a>
+
+                    <a href="#" class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Attach image</a>
+                </div>
+            </div>
+
+            <div class="p-4 bg-white border border-gray-200 rounded-lg">
+                <div class="mb-6 flex items-center justify-between">
+                    <div class="flex items-center space-x-6">
+                        <img src="https://i.pravatar.cc/300?img=26" class="w-[40px] rounded-full">
+
+                        <p><strong>Maria Stein</strong></p>
+                    </div>
+
+                    <p class="text-gray-600">18 minutes ago</p>
+                </div>
+
+                <img class="w-full rounded-lg" src="https://images.unsplash.com/photo-1678434763328-a20bae5940a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60">
+                
+                <div class="my-6 flex justify-between">
+                    <div class="flex space-x-6">
+                        <div class="flex items-center space-x-2">
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                            <span class="text-gray-500 text-xs">82 likes</span>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <svg width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_429_11233)">
+<path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 13.4876 3.36093 14.891 4 16.1272L3 21L7.8728 20C9.10904 20.6391 10.5124 21 12 21Z" stroke="#292929" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_429_11233">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+                            </svg>      
+                            <span class="text-gray-500 text-xs">0 comments</span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5Z" fill="#000000"/>
+                        <path d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" fill="#000000"/>
+                        <path d="M12 21C13.1046 21 14 20.1046 14 19C14 17.8954 13.1046 17 12 17C10.8954 17 10 17.8954 10 19C10 20.1046 10.8954 21 12 21Z" fill="#000000"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-4 bg-white border border-gray-200 rounded-lg">
+                <div class="mb-6 flex items-center justify-between">
+                    <div class="flex items-center space-x-6">
+                        <img src="https://i.pravatar.cc/300?img=42" class="w-[40px] rounded-full">
+
+                        <p><strong>Maria Stein</strong></p>
+                    </div>
+
+                    <p class="text-gray-600">18 minutes ago</p>
+                </div>
+                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestiae necessitatibus, dolores esse tenetur unde vero quisquam alias, libero nisi consequuntur eaque, aut porro sunt eveniet vitae? Suscipit, et sunt!</p> 
+                
+                <div class="my-6 flex justify-between">
+                    <div class="flex space-x-6">
+                        <div class="flex items-center space-x-2">
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                            <span class="text-gray-500 text-xs">82 likes</span>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <svg width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_429_11233)">
+                                <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 13.4876 3.36093 14.891 4 16.1272L3 21L7.8728 20C9.10904 20.6391 10.5124 21 12 21Z" stroke="#292929" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                                <defs>
+                                <clipPath id="clip0_429_11233">
+                                <rect width="24" height="24" fill="white"/>
+                                </clipPath>
+                                </defs>
+                            </svg>      
+                            <span class="text-gray-500 text-xs">0 comments</span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5Z" fill="#000000"/>
+                        <path d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" fill="#000000"/>
+                        <path d="M12 21C13.1046 21 14 20.1046 14 19C14 17.8954 13.1046 17 12 17C10.8954 17 10 17.8954 10 19C10 20.1046 10.8954 21 12 21Z" fill="#000000"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="main-right col-span-1 space-y-4">
+            <PeopleYouMayKnow/>
+            <Trends/>
+        </div>
+    </div>
+</template>
+
+<script>
+import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
+import Trends from '../components/Trends.vue'
+export default {
+    name: 'FeedView',
+    components: {
+        PeopleYouMayKnow,
+        Trends,
+    }
+}
+</script>
