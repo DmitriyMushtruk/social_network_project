3dbf71f3e1d52472d1e0890a9c8b1b0586ba9841
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-08-06 22:18:59 +0200

    fix: typos in code
    removed unnecessary launch of entrypoint.sh

--- social_backend/social_backend/settings.py
+++ social_backend/social_backend/settings.py
@@ -8,9 +8,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
