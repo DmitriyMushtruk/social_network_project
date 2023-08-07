6ff314dd102ef66b4ee24cfaa1c476d61c0e9d7c
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-08-07 23:10:44 +0200

    refactor: removed custom user manager
    Removed custom user manager & crate migrations

--- social_backend/account/models.py
+++ social_backend/account/models.py
@@ -7,7 +7,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from .managers import CustomUserManager
 from django.template.defaultfilters import slugify
 
 class User(AbstractUser):
@@ -21,16 +20,11 @@ class User(AbstractUser):
     image_s3_path = models.CharField(max_length=200, null=True, blank=True)
     role = models.CharField(max_length=9, choices=Roles.choices, default=Roles.USER)
     
-    username = models.CharField(max_length=80)
+    username = models.CharField(max_length=80, blank=False, unique=True)
     is_blocked = models.BooleanField(default=False)
 
     date_joined = models.DateTimeField(default=timezone.now)
     last_login = models.DateTimeField(blank=True, null=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
-
-    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
