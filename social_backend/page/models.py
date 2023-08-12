29f9330bdb2a289c29c7234bfab2703d57402920
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-08-12 23:31:28 +0200

    feat: created models for page
    Creating models Tag and Page

--- social_backend/page/models.py
+++ social_backend/page/models.py
@@ -7,43 +7,28 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class Page(models.Model):
     name = models.CharField(max_length=80)
+    owner = models.ForeignKey('account.User', on_delete=models.CASCADE, related_name='pages', blank=False)
     uuid = models.CharField(max_length=30, unique=True)
     description = models.TextField()
     tags = models.ManyToManyField('page.Tag', related_name='pages')
 
-    owner = models.ForeignKey('account.User', on_delete=models.CASCADE, related_name='pages', blank=False)
-    followers = models.ManyToManyField('page.Page', through='page.PageFollows', related_name='follows', blank=True)
+    is_private = models.BooleanField(default=False)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='page_follows', blank=True)
+    follow_requests = models.ManyToManyField('self',blank=True,related_name='requests_to_follow',symmetrical=False)
 
     image = models.URLField(null=True, blank=True)
-
-    is_private = models.BooleanField(default=False)
-    follow_requests = models.ManyToManyField('page.Page', related_name='requests', blank=True)
-
     unblock_date = models.DateTimeField(null=True, blank=True)
 
+    #Get number of page followers
+    def count_followers(self):
+        return self.followers.count()
+    
+    #Get number of page follows
+    def count_following(self):
+        return Page.objects.filter(followers=self).count()
+    
     def __str__(self):
         return f"{self.name}"
-    
-class PageFollows(models.Model):
-    from_page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='follows_from')
-    to_page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='follows_to')
-
-class Post(models.Model):
-    page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='posts')
-    content = models.CharField(max_length=250)
-
-    likes = models.ManyToManyField('page.Page', related_name="post_likes", blank=True)
-    reply_to = models.ForeignKey('page.Post', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ("-created_at",)
-
-    def __str__(self):
-        return f"{self.page.name}" f"{self.page}"
-    
