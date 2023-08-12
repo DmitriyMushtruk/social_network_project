881d4e0948d5ec6f0f8ec01debcbf1d51a02a488
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-08-12 23:33:40 +0200

    feat: creating & configure admin.py  file

--- social_backend/page/admin.py
+++ social_backend/page/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Page
+
+admin.site.register(Tag)
+
+class TagInline(admin.StackedInline):
+    model = Tag
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner","get_followers_count", "get_following_count", "is_private")
+
+    def get_followers_count(self, obj):
+        return obj.count_followers()
+    
+    def get_following_count(self, obj):
+        return obj.count_following()
+
+    get_followers_count.short_description = 'Followers Count'
+    get_following_count.short_description = 'Following Count'
