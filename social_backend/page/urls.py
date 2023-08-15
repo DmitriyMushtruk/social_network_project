165047f7cf32a6c150b0e9194f297d0217e5a8c4
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-08-15 11:05:48 +0200

    feat: update ursl.py for new views

--- social_backend/page/urls.py
+++ social_backend/page/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import TagListViewSet, UserPageListViewSet, PageViewSet
+from .views import TagListViewSet, UserPageListViewSet, PageViewSet, PostViewSet
 
 app_name = "posts"
 
@@ -10,6 +10,7 @@ router = DefaultRouter()
 router.register(r"tags", TagListViewSet)
 router.register(r"user_pagelist", UserPageListViewSet)
 router.register(r'page', PageViewSet, basename='page')
+router.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
     path("", include(router.urls)),
