89a8c664dfeb8fde9c15b0e86d7918d76035259f
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-08-23 13:02:24 +0200

    feat: created permissions file
    Created permissions: IsOwner, CanViewPage, CanViewPost.

--- social_backend/page/page_permissions.py
+++ social_backend/page/page_permissions.py
@@ -1,21 +1,38 @@
 from rest_framework import permissions
-from .models import Page
+from .models import Page, Post
+from account.models import User
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        if isinstance(obj, Page):
+            return obj.owner == request.user
+        if isinstance(obj, Post):
+            return obj.page.owner == request.user
 
 class CanViewPage(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        user = request.user
         if obj.is_private:
             current_page = request.query_params.get('current_page')
-            return obj.followers.filter(id=current_page).exists()
-        elif obj.owner == request.user:
-            return True
+            if obj.followers.filter(id=current_page).exists():
+                return True
+            elif obj.owner == request.user:
+                return True
+            elif user.role == User.Roles.MODERATOR or user.role == User.Roles.ADMIN:
+                return True
         else:
             return True
-    
 
-    #user_role = request.user.role
-        # if request.user.role.MODERATOR or request.user.role.ADMIN:
-        #     return True
+class CanViewPost(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if obj.page.is_private:
+            current_page = request.query_params.get('current_page')
+            if obj.page.followers.filter(id=current_page).exists():
+                return True
+            elif user == obj.page.owner:
+                return True
+            elif  user.role == User.Roles.MODERATOR or user.role == User.Roles.ADMIN:
+                return True
+        else:
+            return True
