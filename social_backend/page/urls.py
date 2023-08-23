7b89e131960e7dd94800d05462af2e416cea5bce
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-08-23 13:07:54 +0200

    feat: created new path for follow requests
    deleted unusable register
    created new register used PostsViewSet

--- social_backend/page/urls.py
+++ social_backend/page/urls.py
@@ -2,22 +2,22 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 
-from .views import TagListViewSet, UserPageListViewSet, PageViewSet
+from .views import TagListViewSet, UserPageListViewSet, PageViewSet, PostViewSet
 
 app_name = "page"
 
 router = DefaultRouter()
-router.register(r'posts', PageViewSet, basename='pages-posts')
+
 
 router.register(r"tags", TagListViewSet)
 router.register(r"userpagelist", UserPageListViewSet, basename='userpagelist')
 router.register(r'pages', PageViewSet, basename='pages')
-
+router.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("pages/<str:name>/posts/<int:pk>/", PageViewSet.as_view({"get": "posts", "post": "posts", "put": "posts", "delete": "posts"}), name="post-detail"),
-    path("pages/<str:name>/approve_request/<int:pk>/", PageViewSet.as_view({"post": "approve_request"}), name="page-approve_request"),
+    path('pages/<str:name>/requests/approve/<int:id_page>/', PageViewSet.as_view({'post': 'approve_request'}), name='approve-request'),
+    path('pages/<str:name>/requests/reject/<int:id_page>/', PageViewSet.as_view({'post': 'reject_request'}), name='reject-request'),
  ]
 
 urlpatterns += router.urls
