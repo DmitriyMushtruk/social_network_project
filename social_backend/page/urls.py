6a4283a5eef579f7789fc59387f65ae2d520feb0
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-09-02 21:29:38 +0200

    feat: changed urls
    Urls have been changed, a separate action has been added to each url

--- social_backend/page/urls.py
+++ social_backend/page/urls.py
@@ -14,8 +14,40 @@ router.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('pages/<str:name>/requests/approve/<int:id_page>/', PageViewSet.as_view({'post': 'approve_request'}), name='approve-request'),
-    path('pages/<str:name>/requests/reject/<int:id_page>/', PageViewSet.as_view({'post': 'reject_request'}), name='reject-request'),
+
+    path('pages/<str:name>/requests/',
+        PageViewSet.as_view({'get': 'get_requests_action'}), name='requests'),
+    path(
+        'pages/<str:name>/requests/approve/<int:requester_page>/',
+        PageViewSet.as_view({'post': 'approve_requests_action'}),
+        name='approve_request'
+        ),
+    path(
+        'pages/<str:name>/requests/approve/',
+        PageViewSet.as_view({'post': 'approve_requests_action'}),
+        kwargs={"requester_page": None}, name='approve_requests'
+        ),
+    path(
+        'pages/<str:name>/requests/reject/<int:requester_page>/',
+        PageViewSet.as_view({'post': 'reject_requests_action'}),
+        name='reject_request'
+        ),
+    path(
+        'pages/<str:name>/requests/reject/',
+        PageViewSet.as_view({'post': 'reject_requests_action'}),
+        kwargs={"requester_page": None}, name='reject_requests'
+        ),
+    path('pages/<str:name>/followers/',
+        PageViewSet.as_view({'get': 'get_followers_action'}), name='followers'),
+    path('pages/<int:pk>/block/', PageViewSet.as_view({'post': 'block'}), name='block-page'),
+    path('pages/<int:pk>/unblock/', PageViewSet.as_view({'post': 'unblock'}), name='unblock-page'),
+
+    path('posts/list/<str:name>/', PostViewSet.as_view({'get': 'get_page_posts_action'}), name='posts_list'),
+    path('posts/<int:pk>/like/', PostViewSet.as_view({'post': 'like_action'}), name='like_post'),
+    path('posts/<int:pk>/dislike/', PostViewSet.as_view({'post': 'dislike_action'}), name='dislike_post'),
+    path('posts/<int:pk>/comment/', PostViewSet.as_view({'post': 'comment_action'}), name='comment_post'),
+    path('posts/<int:pk>/repost/', PostViewSet.as_view({'post': 'repost_action'}), name='repost_post'),
+
     path('feeds/', FeedView.as_view(), name='feeds'),
  ]
 
