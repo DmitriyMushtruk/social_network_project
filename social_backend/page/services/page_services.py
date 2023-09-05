77f8f2bd33c0dd51ce2b0a7fc789a39a4ecd5f2e
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-09-05 21:52:48 +0200

    refactor: updated services
    Created two separate files for page-view and post-view instead of one

---
+++ social_backend/page/services/page_services.py
@@ -0,0 +1,74 @@
+from rest_framework.response import Response
+from account.models import User
+from page.models import Page, Post
+from django.db.models import Q
+
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_404_NOT_FOUND,
+    HTTP_409_CONFLICT,
+)
+
+def follow_unfollow(current_page: Page, page = Page, user = User) -> Response:
+    if page in user.pages.all():
+        return Response(
+            {"response": "You're can't follow your own page"},
+            status=HTTP_409_CONFLICT,
+        )
+
+    if current_page in page.followers.all():
+        page.followers.remove(current_page)
+        return Response({"detail": "Unfollowed successfully."},status=HTTP_200_OK,)
+    else:
+        if page.is_private:
+            if not current_page in page.follow_requests.all():
+                page.follow_requests.add(current_page)
+                return Response({'detail': 'Follow request sent successfully.'}, status=HTTP_200_OK)
+            else:
+                page.follow_requests.remove(current_page)
+                return Response({'detail': 'Follow request canceled successfully.'}, status=HTTP_200_OK)
+        else: 
+            page.followers.add(current_page)
+            return Response({'detail': 'Followed successfully.'}, status=HTTP_200_OK)
+        
+        
+def approve_request(page: Page, requester: Page) -> Response:
+    if requester in page.follow_requests.all():
+        page.followers.add(requester)
+        page.follow_requests.remove(requester)
+        return Response({'detail': 'Request approved successfully.'}, status=HTTP_200_OK)
+    else:
+        return Response({'detail': 'Follow request not found.'}, status=HTTP_404_NOT_FOUND)
+    
+def approve_all_requests(page: Page) -> Response:
+    requesters = page.follow_requests.all()
+    for requester in requesters:
+        page.followers.add(requester)
+        page.follow_requests.remove(requester)
+    return Response({'detail': 'All requests approved successfully.'}, status=HTTP_200_OK)
+    
+def reject_request(page: Page, requester: Page) -> Response:
+    if requester in page.follow_requests.all():
+        page.follow_requests.remove(requester)
+        return Response({'detail': 'Request reject successfully.'}, status=HTTP_200_OK)
+    else:
+        return Response({'detail': 'Follow request not found.'}, status=HTTP_404_NOT_FOUND)
+    
+def reject_all_requests(page: Page) -> Response:
+    requesters = page.follow_requests.all()
+    for requester in requesters:
+        page.follow_requests.remove(requester)
+    return Response({'detail': 'All requests reject successfully.'}, status=HTTP_200_OK)
+
+def get_page_posts(page: Page, user: User, current_page: Page) -> Response:
+    condition = (
+        Q(page=page, reply_to__isnull=True, page__unblock_date__isnull=True) |  # Обычные посты на странце
+        Q(page=page, reply_to__isnull=False, reply_to__page__unblock_date__isnull=True) & ~Q(reply_to__page=page)  # Репосты из открытых страниц
+    )
+
+    if user.role not in [User.Roles.MODERATOR, User.Roles.ADMIN]:
+        posts = Post.objects.filter(condition)
+    else:
+        posts = Post.objects.filter(page=page)
+
+    return posts.order_by('-created_at')
---
+++ social_backend/page/services/post_services.py
@@ -0,0 +1,64 @@
+from rest_framework.response import Response
+from account.models import User
+from page.models import Page, Post
+from django.db.models import Q
+
+from rest_framework.status import (
+    HTTP_403_FORBIDDEN,
+    HTTP_409_CONFLICT,
+    HTTP_201_CREATED,
+)
+
+
+def get_feeds(user: User, current_page: Page) -> Response:
+    condition = (
+        Q(page__owner=user, reply_to__isnull=True, page__unblock_date__isnull=True) | # Обычные посты собственных страниц
+        Q(page__followers=current_page, reply_to__isnull=True, page__unblock_date__isnull=True) | # Обычные посты страниц, на которые подписана текущая страница
+        Q(page__owner=user, reply_to__isnull=False, reply_to__page__is_private=False,  page__unblock_date__isnull=True) # Посты-репосты собственных страниц
+    )
+
+    if user.role in [User.Roles.MODERATOR, User.Roles.ADMIN]:
+        posts = Post.objects.filter(condition)
+    else:
+        posts = Post.objects.filter(condition)
+        
+    return posts.order_by('-created_at')
+
+
+def repost_post(original_post: Post, current_page: Page) -> Response:
+    if current_page.posts.filter(id=original_post.id).exists():
+        return Response({'detail': 'Repost failed.'}, status=HTTP_409_CONFLICT)
+    elif original_post.page.is_private:
+        return Response({'detail': 'Repost failed. Repost from private page is not allowed.'}, status=HTTP_403_FORBIDDEN)
+    
+    Post.objects.create(
+    page=current_page,
+    content=original_post.content,
+    file=original_post.file,
+    reply_to=original_post,
+    )
+    return Response({'detail': 'Reposted successfully.'}, status=HTTP_201_CREATED)
+
+def like_post(post: Post, current_page: Page) -> Response:
+    if post.dislikes.filter(author=current_page).first():
+        post.dislikes.filter(author=current_page).delete()
+        post.likes.create(author=current_page)
+        return Response({"message": "Dislike removed, Like added"}, status=201)
+    elif post.likes.filter(author=current_page).exists():
+        post.likes.filter(author=current_page).delete()
+        return Response({"message": "Like removed"}, status=200)
+    else:
+        post.likes.create(author=current_page)
+        return Response({"message": "Like added"}, status=201)
+    
+def dislike_post(post: Post, current_page: Page) -> Response:
+    if post.likes.filter(author=current_page).first():
+        post.likes.filter(author=current_page).delete()
+        post.dislikes.create(author=current_page)
+        return Response({"message": " Like removed, Dislike added"}, status=201)
+    elif post.dislikes.filter(author=current_page).exists():
+        post.dislikes.filter(author=current_page).delete()
+        return Response({"message": "Dislike removed"}, status=200)
+    else:
+        post.dislikes.create(author=current_page)
+        return Response({"message": "Dislike added"}, status=201)
