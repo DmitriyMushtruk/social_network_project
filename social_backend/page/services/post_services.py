556cb00070f46ce4873cd8ad4970a9edbb994cd9
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-09-11 00:05:31 +0200

    style: change post_services

--- social_backend/page/services/post_services.py
+++ social_backend/page/services/post_services.py
@@ -12,34 +12,42 @@ from rest_framework.status import (
 
 def get_feeds(user: User, current_page: Page) -> Response:
     condition = (
-        Q(page__owner=user, reply_to__isnull=True, page__unblock_date__isnull=True) | # Обычные посты собственных страниц
-        Q(page__followers=current_page, reply_to__isnull=True, page__unblock_date__isnull=True) | # Обычные посты страниц, на которые подписана текущая страница
-        Q(page__owner=user, reply_to__isnull=False, reply_to__page__is_private=False,  page__unblock_date__isnull=True) # Посты-репосты собственных страниц
+        # Обычные посты собственных страниц
+        Q(page__owner=user, reply_to__isnull=True, page__unblock_date__isnull=True) |
+        # Обычные посты страниц, на которые подписана текущая страница
+        Q(page__followers=current_page, reply_to__isnull=True, page__unblock_date__isnull=True) |
+        # Посты-репосты собственных страниц
+        Q(page__owner=user, reply_to__isnull=False, reply_to__page__is_private=False,
+          page__unblock_date__isnull=True)
     )
 
     if user.role in [User.Roles.MODERATOR, User.Roles.ADMIN]:
         posts = Post.objects.filter(condition)
     else:
         posts = Post.objects.filter(condition)
-        
+
     return posts.order_by('-created_at')
 
 
 def repost_post(original_post: Post, current_page: Page) -> Response:
     if current_page.posts.filter(reply_to_id=original_post.id).exists():
-        return Response({'detail': 'Repost failed. This post has already been reposted on your page.'}, status=HTTP_409_CONFLICT)
+        return Response({'detail': 'Repost failed. This post has already been reposted on your page.'},
+                        status=HTTP_409_CONFLICT)
     elif original_post.page.is_private:
-        return Response({'detail': 'Repost failed. Repost from private page is not allowed.'}, status=HTTP_403_FORBIDDEN)
+        return Response({'detail': 'Repost failed. Repost from private page is not allowed.'},
+                        status=HTTP_403_FORBIDDEN)
     elif current_page == original_post.page:
-        return Response({'detail': 'Repost failed. Reposting your own post is not allowed.'}, status=HTTP_403_FORBIDDEN)
-    
+        return Response({'detail': 'Repost failed. Reposting your own post is not allowed.'},
+                        status=HTTP_403_FORBIDDEN)
+
     Post.objects.create(
-    page=current_page,
-    content=original_post.content,
-    file=original_post.file,
-    reply_to=original_post,
+        page=current_page,
+        content=original_post.content,
+        file=original_post.file,
+        reply_to=original_post,
     )
     return Response({'detail': 'Reposted successfully.'}, status=HTTP_201_CREATED)
+
 
 def like_post(post: Post, current_page: Page) -> Response:
     if post.dislikes.filter(author=current_page).first():
@@ -52,7 +60,8 @@ def like_post(post: Post, current_page: Page) -> Response:
     else:
         post.likes.create(author=current_page)
         return Response({"message": "Like added"}, status=201)
-    
+
+
 def dislike_post(post: Post, current_page: Page) -> Response:
     if post.likes.filter(author=current_page).first():
         post.likes.filter(author=current_page).delete()
