a0c83227a0dd30eb8943101da0590b6663655bac
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-09-17 00:55:13 +0200

    feat(email_sender): create send message logic

---
+++ social_backend/page/email_sender.py
@@ -0,0 +1,26 @@
+from celery import shared_task
+from django.core.mail import EmailMessage
+from social_backend import settings
+
+from page.models import Post
+from django.core.mail import send_mail
+
+
+@shared_task
+def send_new_post_notification_email(post_id: int) -> None:
+    post = Post.objects.get(pk=post_id)
+
+    recipients_emails = []
+    for page in post.page.followers.all():
+        if (page.owner.email
+                not in recipients_emails
+                and not page.owner.is_blocked):
+            recipients_emails.append(page.owner.email)
+
+    subject = f'Check out new post by {post.page.owner.username}'
+    message = f'New post is available on {post.page.name} page! \U0001F680'
+    from_email = settings.EMAIL_HOST_USER
+
+    send_mail(subject, message, from_email, recipients_emails, fail_silently=False)
+
+
