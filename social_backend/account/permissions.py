7e503609cee78e77e0e416aa5cdbec40848e709e
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2023-09-22 22:43:31 +0200

    chore(account.permissions): add new permissions
    Namely: IsNotAuthenticated, IsAuthenticatedOrReadOnly

--- social_backend/account/permissions.py
+++ social_backend/account/permissions.py
@@ -2,6 +2,22 @@ from .models import User
 from rest_framework import permissions
 
 
+class IsNotAuthenticated(permissions.BasePermission):
+    """Checks if user is authenticated"""
+    def has_permission(self, request, view):
+        return not request.user.is_authenticated
+
+
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    """The request is authenticated as a user, or is a read-only request."""
+    def has_permission(self, request, view):
+        return bool(
+            request.method in ('GET', 'HEAD', 'OPTIONS') or
+            request.user and
+            request.user.is_authenticated
+        )
+
+
 class IsAdmin(permissions.BasePermission):
     """ Checks if the user have an admin role """
 
